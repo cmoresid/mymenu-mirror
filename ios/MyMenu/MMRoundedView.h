05afe6c5fa8f040f4a09404f46bb26b16261c3b7
klafond <klafond@ualberta.ca>
2014-01-27 13:58:38 -0700

    added rounded buttons

---
+++ ios/MyMenu/MMRoundedView.h
@@ -0,0 +1,13 @@
+//
+//  MMRoundedView.h
+//  MyMenu
+//
+//  Created by ninjavmware on 2014-01-27.
+//  Copyright (c) 2014 MyMenu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMRoundedView : UIView
+
+@end
---
+++ ios/MyMenu/MMRoundedView.m
@@ -0,0 +1,36 @@
+//
+//  MMRoundedView.m
+//  MyMenu
+//
+//  Created by ninjavmware on 2014-01-27.
+//  Copyright (c) 2014 MyMenu. All rights reserved.
+//
+
+#import "MMRoundedView.h"
+
+@implementation MMRoundedView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+	self.layer.cornerRadius = 5;
+	self.layer.masksToBounds = YES;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
