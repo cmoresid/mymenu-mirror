daa7fa9cb6a8437028e2fbbac4e6a4e45aab515e
Prateek Srivastava <f2prateek@gmail.com>
2014-01-30 18:13:49 -0700

    View specials

--- ios/MyMenu/MMSpecialsCollectionViewController.m
+++ ios/MyMenu/MMSpecialsCollectionViewController.m
@@ -8,7 +8,7 @@
 
 #import "MMSpecialsCollectionViewController.h"
 #import "MMSpecial.h"
-#import "MMRestriction.h"
+#import "MMDBFetcher.h"
 
 @interface MMSpecialsCollectionViewController ()
 @property(weak, nonatomic) IBOutlet UISegmentedControl *weekDayButtons;
@@ -16,36 +16,57 @@
 
 @implementation MMSpecialsCollectionViewController
 
-// test
 NSArray *specials;
+static NSString *days[] = {@"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday", @"Sunday"};
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
     if (self) {
         // Custom initialization
-        // First Time Loading
-        NSDate *date = [NSDate date];
-        // Get Day of week here
-        // Set the button as current
     }
+
     return self;
+}
+
+- (IBAction)dayChanged:(UISegmentedControl *)sender {
+    NSUInteger index = (NSUInteger) sender.selectedSegmentIndex;
+
+    if (UISegmentedControlNoSegment != index) {
+        NSString *day = days[index];
+        NSLog(@"at index, %u, with day %@", index, day);
+        [self loadDay:day];
+        [[self collectionView] reloadData];
+    }
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    MMRestriction *restriction = [[MMRestriction alloc] init];
-    //[restriction setImage:[UIImage imageNamed:@"rest.jpg"]];
-    [restriction setName:@"Drinks"];
-    // Use specials type to determine if should be showed for this
-    // Type. Next Filter By day of the week.
-    //[self specialsType];
-    MMSpecial *special = [[MMSpecial alloc] init];
-    [special setName:@"Title Goes Here"];
-    [special setDesc:@"This is the Description for this item... We should test large ammounts of text.."];
-    [special setPicture:@"rest.png"];
 
-    specials = [NSArray arrayWithObjects:special, special, special, nil];
+    NSString *day = [self getToday];
+    int index;
+
+    for (int i = 0; i < 7; i++) {
+        if ([[days[i] lowercaseString] isEqualToString:[day lowercaseString]]) {
+            index = i;
+            NSLog(@"today is at index %d", index);
+            break;
+        }
+    }
+
+    [self.tabOutlet setSelectedSegmentIndex:index];
+    [self loadDay:[self getToday]];
+}
+
+- (NSString *)getToday {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEEE"];
+    return [[dateFormatter stringFromDate:[NSDate date]] lowercaseString];
+}
+
+- (void)loadDay:(NSString *)day {
+    specials = [[MMDBFetcher get] getSpecials:day :self.specialsType];
+    NSLog(@"got %d specials for %@ with type %d.", specials.count, day, self.specialsType);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -60,40 +81,36 @@ NSArray *specials;
     return specials.count;
 }
 
-
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *identifier = @"Cell";
 
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
 
     // Rounded Corners
-
     [cell.contentView setBackgroundColor:[UIColor whiteColor]];
 
     cell.contentView.layer.cornerRadius = 5;
     cell.contentView.layer.masksToBounds = YES;
 
-    MMSpecial *restriction = [specials objectAtIndex:indexPath.row];
+    MMSpecial *special = [specials objectAtIndex:indexPath.row];
+    UIImage *myImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:special.picture]]];
 
-    [(UIImageView *) [cell viewWithTag:100] setImage:[UIImage imageNamed:@"rest.jpg"]];
+    [(UIImageView *) [cell viewWithTag:100] setImage:myImage];
 
     // Set the Restriction Title
     UITextView *textView = (UITextView *) [cell viewWithTag:101];
     UITextView *textDesc = (UITextView *) [cell viewWithTag:102];
-    textView.text = restriction.name;
-    textDesc.text = restriction.desc;
+    textView.text = special.name;
+    textDesc.text = special.desc;
     [textDesc sizeToFit];
-
 
     return cell;
 }
-
 
 // I implemented didSelectItemAtIndexPath:, but you could use willSelectItemAtIndexPath: depending on what you intend to do. See the docs of these two methods for the differences.
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     // If you need to use the touched cell, you can retrieve it like so
     UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
-
     NSLog(@"touched cell %@ at indexPath %@", cell, indexPath);
 }
 
