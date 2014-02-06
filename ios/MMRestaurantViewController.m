f24fc16fd5235545b3463260c198a2387092f4a6
klafond <klafond@ualberta.ca>
2014-02-06 14:20:28 -0700

    hassans stuff

--- ios/MMRestaurantViewController.m
+++ ios/MMRestaurantViewController.m
@@ -15,12 +15,18 @@
 //  along with this program.  If not, see [http://www.gnu.org/licenses/].
 
 #import "MMRestaurantViewController.h"
+#import "MMMenuItem.h"
+#import "MBProgressHUD.h"
+//#import "SDWebImage/UIImageView+WebCache.h"
+
 
 @interface MMRestaurantViewController ()
 
 @end
 
 @implementation MMRestaurantViewController
+
+static NSString * menuCateogires = {@"Dinner", @"Breakfast", @"Lunch", @"Dessert", @"Drink", @"Appetizer"};
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -34,14 +40,21 @@
 - (void)viewDidLoad
 {	
     [super viewDidLoad];
-    NSLog(@"Restaurant Name = @%@" , _selectedRestaurant.businessname);
+    //NSLog(@"Restaurant Name = @%@" , _selectedRestaurant.businessname);
     _restName.text = _selectedRestaurant.businessname;
     _restNumber.text = _selectedRestaurant.phone;
-    _restRating.text = [_selectedRestaurant.rating stringValue];
+    
+    	
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    [formatter setRoundingMode:NSNumberFormatterRoundHalfUp];
+    [formatter setMaximumFractionDigits:3];
+    //NSLog(@"%@",[formatter  stringFromNumber:_selectedRestaurant.rating]);
+    _restRating.text = [formatter  stringFromNumber:_selectedRestaurant.rating];
+    
+    
     _restDescription.text = _selectedRestaurant.desc;
     _restImage.image = [UIImage imageWithData:                                                                      [NSData dataWithContentsOfURL:                                                                            [NSURL URLWithString: _selectedRestaurant.picture]]];
-    
-    NSLog(@"?????");
+
 
 	// Do any additional setup after loading the view.
 }
@@ -57,4 +70,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _menuItems.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"Cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    // Rounded Corners
+    cell.contentView.backgroundColor = [UIColor whiteColor];
+    cell.contentView.layer.cornerRadius = 5;
+    cell.contentView.layer.masksToBounds = YES;
+    
+    MMMenuItem *menitem = [_menuItems objectAtIndex:indexPath.row];
+    
+    UIImageView *imageView = (UIImageView *) [cell viewWithTag:100];
+    //[imageView setImageWithURL:[NSURL URLWithString:[menitem picture]] placeholderImage:[UIImage imageNamed:@"restriction_placeholder.png"]];
+    
+    // Set the text
+    UITextView *textView = (UITextView *) [cell viewWithTag:101];
+    UITextView *textDesc = (UITextView *) [cell viewWithTag:102];
+    textView.text = menitem.name;
+    textDesc.text = menitem.desc;
+    [textDesc sizeToFit];
+    
+    return cell;
+}
+
+// I implemented didSelectItemAtIndexPath:, but you could use willSelectItemAtIndexPath: depending on what you intend to do. See the docs of these two methods for the differences.
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    // If you need to use the touched cell, you can retrieve it like so
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
+    NSLog(@"touched cell %@ at indexPath %@", cell, indexPath);
+}
 @end
