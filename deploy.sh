534a257dfb60d73e7eae106efbb8ad81cc704ff6
Prateek Srivastava <f2prateek@gmail.com>
2014-03-01 21:10:45 -0700

    Add a deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,24 @@
 
 set -ex
 
-# check if there are any dirty files
-git diff --exit-code
-echo $?
+if [ -n "$(git status --porcelain)" ]; then
+  echo "There are changes. Commit them first.";
+  exit 1
+else
+  echo "Proceeding with release.";
+fi
+
+# generate a changelog
+github-changes -o UniversityOfAlberta -r MyMenu -a
+
+# commit the changelog, and offically commit the release
+git add -A
+git commit -m 'Release $1'
+git tag '$1'
+
+# push upstream
+git push -u origin master
+git push --tags
+
+# deploy the website
+./deploy_website.sh
