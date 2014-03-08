6a9ceadb760de1effcb342a8aa1157a7b8d05090
Connor Moreside <cmoresid@ualberta.ca>
2014-03-08 15:01:43 -0700

    Added some documentation. Added license information to code files. Changed global tint to teal so user current location looks better.

--- ios/MyMenu/Classes/Service/MMLoginManager.h
+++ ios/MyMenu/Classes/Service/MMLoginManager.h
@@ -1,19 +1,52 @@
 //
-//  MMLoginManager.h
-//  MyMenu
+//  Copyright (C) 2014  MyMenu, Inc.
 //
-//  Created by Connor Moreside on 3/6/2014.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see [http://www.gnu.org/licenses/].
 //
 
 #import <Foundation/Foundation.h>
 #import "MMDBFetcherDelegate.h"
 
+/**
+ *  Notification identifer that refers to the notification 
+ *  that is sent when a user is logged.
+ */
 extern NSString *const kUserLoginNotification;
+
+/**
+ *  Notification identifer that refers to the notification
+ *  that is sent when a user login error occurs. Generally,
+ *  this would be due to a network error.
+ */
 extern NSString *const kUserLoginErrorNotification;
+
+/**
+ *  Notification identifer that refers to the notification
+ *  that is sent when a user profile has been updated.
+ */
 extern NSString *const kUserUpdatedNotification;
+
+/**
+ *  Notification identifer that refers to the notification
+ *  that is sent when a user profile cannot be updated due
+ *  to an error. Generally, this is due to a network error.
+ */
 extern NSString *const kUserUpdateErrorNotification;
 
+/**
+ *  <#Description#>
+ */
 @interface MMLoginManager : NSObject <MMDBFetcherDelegate>
 
 + (id)sharedLoginManager;
