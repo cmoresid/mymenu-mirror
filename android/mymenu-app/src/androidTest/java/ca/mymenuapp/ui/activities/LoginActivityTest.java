a8d2f92df4d74d5556c7630d21ad6809c5ee98b0
Prateek Srivastava <f2prateek@gmail.com>
2014-03-13 23:23:40 -0600

    Add Stub for tests
    Drop beta builds for now, speeds up full builds by a bit

---
+++ android/mymenu-app/src/androidTest/java/ca/mymenuapp/ui/activities/LoginActivityTest.java
@@ -0,0 +1,33 @@
+package ca.mymenuapp.ui.activities;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+import ca.mymenuapp.R;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+  @SuppressWarnings("deprecation")
+  public LoginActivityTest() {
+    // This constructor was deprecated - but we want to support lower API levels.
+    super("com.google.android.apps.common.testing.ui.testapp", LoginActivity.class);
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    // Espresso will not launch our activity for us, we must launch it via getActivity().
+    getActivity();
+  }
+
+  @SuppressWarnings("unchecked")
+  public void testClickLogin() {
+    onView(withId(R.id.login)).perform(click());
+    onView(withId(R.id.login)).check(matches(withText("Login")));
+  }
+}
