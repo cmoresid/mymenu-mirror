a62612c3fd08785e41c6232071f20a8a23b1d9f5
Hassaan <hassaan1@ualberta.ca>
2014-03-28 13:49:26 -0600

    Fixed widget, way smoother, different buttons too.

--- android/mymenu-app/src/main/java/ca/mymenuapp/ui/widgets/RatingWheelView.java
+++ android/mymenu-app/src/main/java/ca/mymenuapp/ui/widgets/RatingWheelView.java
@@ -10,11 +10,12 @@ import android.util.AttributeSet;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
 import ca.mymenuapp.R;
-import com.f2prateek.ln.Ln;
 
 public class RatingWheelView extends View {
+
+  private static final float MAX_RATING = 10f;
+  private static final float MIN_RATING = 1f;
 
   private final RectF tempRect = new RectF();
 
@@ -22,16 +23,13 @@ public class RatingWheelView extends View {
   private final Paint circlePaint = new Paint();
   private final Paint innerCirclePaint = new Paint();
   private final Paint ratingPaint = new Paint();
+  private final Rect textBounds = new Rect();
+
   private float halfTextWidth;
   private float halfTextHeight;
   private double centerX;
   private double centerY;
-  private boolean isSet;
-  private TextView ratingValue;
-
-  private int rating;
-  private int max;
-  private String progressText;
+  private float rating;
 
   public RatingWheelView(Context context) {
     super(context);
@@ -48,6 +46,24 @@ public class RatingWheelView extends View {
     init();
   }
 
+  private void init() {
+    circlePaint.setColor(getResources().getColor(R.color.turqoise));
+    circlePaint.setAntiAlias(true);
+    innerCirclePaint.setColor(0xffffffff);
+    innerCirclePaint.setAntiAlias(true);
+    progressPaint.setColor(getResources().getColor(R.color.pale_green));
+    progressPaint.setAntiAlias(true);
+    ratingPaint.setColor(getResources().getColor(R.color.silver));
+    ratingPaint.setAntiAlias(true);
+    Typeface tf = Typeface.create("Helvetica Neu", Typeface.BOLD);
+    ratingPaint.setTypeface(tf);
+  }
+
+  @Override protected void onFinishInflate() {
+    super.onFinishInflate();
+    setRating(1);
+  }
+
   @Override public boolean onTouchEvent(MotionEvent event) {
     //calculateRating(event.getX(), event.getY());
     // Instantiates the drag shadow builder.
@@ -61,53 +77,36 @@ public class RatingWheelView extends View {
   }
 
   public void calculateRating(float x, float y) {
-    if (isSet) {
-      double deltaX = x - this.centerX;
-      double deltaY = y - this.centerY;
-      double angleRadians = Math.atan(deltaX / deltaY);
+    double deltaX = x - this.centerX;
+    double deltaY = y - this.centerY;
+    double angleRadians = Math.atan(deltaX / deltaY);
 
-      if (angleRadians > 3.1415) angleRadians = 3.1415 - angleRadians;
-
-      double degrees = angleRadians * (180.0 / 3.1415);
-
-      // Check which quadrant the touch occured in and adjust
-      // the angle appropriately.
-      if (x >= centerX && y >= centerY) {
-        degrees = degrees + 180.0;
-      } else if (x >= centerX) {
-        degrees = 270.0 + (90.0 - Math.abs(degrees));
-      } else if (y >= centerY) {
-        degrees = 90.0 + (90.0 - Math.abs(degrees));
-      }
-
-      double percentageOfCircle = (1 - (degrees / 360.0));
-      int finalrating = (int) Math.round(percentageOfCircle * 10);
-
-      setRating(finalrating);
-    } else {
-      Ln.e("Center was not set so input was not dealt with");
+    if (angleRadians > Math.PI) {
+      angleRadians = Math.PI - angleRadians;
     }
-    invalidate();
-  }
 
-  private void init() {
-    circlePaint.setColor(getResources().getColor(R.color.turqoise));
-    circlePaint.setAntiAlias(true);
-    innerCirclePaint.setColor(0xffffffff);
-    progressPaint.setColor(getResources().getColor(R.color.pale_green));
-    progressPaint.setAntiAlias(true);
-    ratingPaint.setColor(getResources().getColor(R.color.silver));
-    Typeface tf = Typeface.create("Helvetica Neu", Typeface.BOLD);
-    ratingPaint.setTypeface(tf);
-    max = 10;
-    setRating(1);
+    double degrees = angleRadians * (180.0 / Math.PI);
+
+    // Check which quadrant the touch occured in and adjust
+    // the angle appropriately.
+    if (x >= centerX && y >= centerY) {
+      degrees = degrees + 180.0;
+    } else if (x >= centerX) {
+      degrees = 270.0 + (90.0 - Math.abs(degrees));
+    } else if (y >= centerY) {
+      degrees = 90.0 + (90.0 - Math.abs(degrees));
+    }
+
+    double percentageOfCircle = (1 - (degrees / 360));
+    setRating((float) (percentageOfCircle * 10));
+
+    invalidate();
   }
 
   @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
     super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    // keep the view square
-    int size = Math.min(getMeasuredWidth(), getMeasuredHeight());
-    setMeasuredDimension(size, size);
+    // keep the view square relative to width
+    setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
   }
 
   @Override public boolean onDragEvent(DragEvent event) {
@@ -115,6 +114,7 @@ public class RatingWheelView extends View {
     switch (event.getAction()) {
       case DragEvent.ACTION_DRAG_LOCATION:
         calculateRating(event.getX(), event.getY());
+        break;
       default:
         break;
     }
@@ -123,28 +123,22 @@ public class RatingWheelView extends View {
 
   @Override protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
-    if (!isSet) {
-      centerX = canvas.getWidth() / 2;
-      centerY = canvas.getHeight() / 2;
-      isSet = true;
-    }
-    if (progressText.equals("0") || rating == 0) {
-      progressText = "1";
-      rating = 1;
-    }
+    centerX = canvas.getWidth() / 2;
+    centerY = canvas.getHeight() / 2;
+
+    String ratingText = String.valueOf(Math.round(rating));
+
     tempRect.set(0, 0, getWidth(), getHeight());
     canvas.drawCircle(tempRect.centerX(), tempRect.centerY(), tempRect.width() / 2, circlePaint);
-    canvas.drawArc(tempRect, -90, 360 * rating / max, true, progressPaint);
+    canvas.drawArc(tempRect, -90, 360 * rating / MAX_RATING, true, progressPaint);
     canvas.drawCircle(tempRect.centerX(), tempRect.centerY(), tempRect.width() / 4,
         innerCirclePaint);
     ratingPaint.setTextSize(tempRect.height() / 3);
 
-    Rect textBounds = new Rect();
-    ratingPaint.getTextBounds(progressText, 0, progressText.length(), textBounds);
-    halfTextWidth = ratingPaint.measureText(progressText) / 2; // Use measureText to calculate width
+    ratingPaint.getTextBounds(ratingText, 0, ratingText.length(), textBounds);
+    halfTextWidth = ratingPaint.measureText(ratingText) / 2; // Use measureText to calculate width
     halfTextHeight = textBounds.height() / 2; // Use height from getTextBounds()
-
-    canvas.drawText(progressText, tempRect.centerX() - halfTextWidth,
+    canvas.drawText(ratingText, tempRect.centerX() - halfTextWidth,
         tempRect.centerY() + halfTextHeight, ratingPaint);
   }
 
@@ -152,10 +146,14 @@ public class RatingWheelView extends View {
    * Sets the current progress and maximum progress value, both of which
    * must be valid values.
    */
-  public void setRating(int rating) {
-    rating = rating > max ? max : rating; // don't let progress be greater than max
-    this.rating = rating;
-    progressText = Integer.toString(rating);
+  public void setRating(float rating) {
+    if (rating < MIN_RATING) {
+      this.rating = MIN_RATING;
+    } else if (rating > MAX_RATING) {
+      this.rating = MAX_RATING;
+    } else {
+      this.rating = rating;
+    }
     invalidate();
   }
 
